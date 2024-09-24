c5491420a8d9f6f3826b64d4e4b4840e15a3aa6f
jrest01 <julian11072000@hotmail.com>
2024-09-24 14:31:47 -0500

    Request and Response Cycle

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():    
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
 def hello():
-    return 'Hello world Flask'
+    user_ip = request.cookies.get('user_ip')
+    return f'Hello world from Flask, your IP address is {user_ip}'
